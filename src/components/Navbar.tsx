57122fc61485993f205ea75b8d650703d8213ef4
sameer <sameersultan1001@gmail.com>
2023-12-28 14:19:17 +0500

    car go

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,26 +1,32 @@
 import { NavLinks } from "@/constants";
 import Image from "next/image";
 import React from "react";
+import BookRideButton from "./BookRideButton";
 
 const Navbar = () => {
   return (
-    <nav className="flex">
+    <nav className="flex px-[2.5rem]  max-lg:justify-between max-lg:px-[2rem] max-xsm:px-[1rem] ">
       <div>
         <Image
-          src={"/logo1.png"}
+          src={"/logo.png"}
           alt={"logo"}
           width={125}
           height={125}
-          className="max-xsm:w-[100px] max-xsm:h-[100px]"
+          className=" h-22 max-xsm:w-[100px]  max-xsm:h-[100px]"
         />
-        <ul>
-        {
-          NavLinks.map(item=> <li key={item.label}> <a href={item.href}>{item.label}</a></li>)
-        }
-        </ul>
-        <div>
-          <button>Book a ride</button>
-        </div>
+      </div>
+      <ul className="flex-1 flex justify-center items-center gap-10 max-lg:hidden">
+        {NavLinks.map((item) => (
+          <li key={item.label} className="nav-links">
+            {" "}
+            <a href={item.href} className=" leading-normal text-lg ">
+              {item.label}
+            </a>
+          </li>
+        ))}
+      </ul>
+      <div>
+        <BookRideButton />
       </div>
     </nav>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,34 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        blue:{
+          light: '#85d7ff',
+          DEFAULT: '#1fb6ff',
+          dark: '#29AFFD',
+          bg:'#E3F0F9'
+        }
+      },
+
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      backgroundSize: {
+        'auto': 'auto',
+        'cover': 'cover',
+        'contain': 'contain',
+        '200%': '200% auto',
+      },
+      backgroundPosition: {
+          'right-center': 'right center'
+      },
+      screens: {
+        'xsm': '375px',
+        'slg': '870px',
+        'xlg': '1250px'
+      }
     },
   },
   plugins: [],
